ebba862d4c309c4fd9e5476fd6e26480eea81b95
matias castro <matias.jca93@gmail.com>
2022-06-10 17:12:10 -0400

    added default value of false for darkMode

--- src/CustomHooks/useDarkMode.js
+++ src/CustomHooks/useDarkMode.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 const useDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage('useDarkMode')
+    const [darkMode, setDarkMode] = useLocalStorage('useDarkMode', false)
     const enabled = darkMode
 
     useEffect(() => {
